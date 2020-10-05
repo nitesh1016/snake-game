a13ce773902f3f0841c97de85f7363270246ab7d
nitesh1016 <niteshprasad809@gmail.com>
2020-10-06 02:16:34 +0530

    added food and score

--- snake.js
+++ snake.js
@@ -3,7 +3,15 @@ function init() {
     w = h = bgcanvas.height = bgcanvas.width = 600;
     pen = bgcanvas.getContext('2d');
     cs = 65;  //cell size
-    
+    game_over = false;
+    score = 5;
+
+    food_img = new Image();
+    food_img.src ="images/apple.png";
+    food = getRandomFood();
+
+    trophy = new Image();
+    trophy.src = "trophy.png"
     snake = {
         init_len : 5,
         color : "red",
@@ -23,10 +31,19 @@ function init() {
            
         },
         updateSnake : function(){
-            console.log("updating snake");
-            this.cells.pop();
             var headX = this.cells[0].x;
             var headY = this.cells[0].y;
+            if(headX=food.x && headY==food.y){
+                console.log("food eaten");
+                food = getRandomFood();
+                score++;
+            }
+            else{
+                this.cells.pop();
+            }
+            console.log("updating snake");
+            
+            
             var nextX,nextY;
             if(this.direction=="right"){
                 nextX = headX + 1;
@@ -46,7 +63,13 @@ function init() {
             }
      
         this.cells.unshift({x:nextX,y:nextY});
-        }
+        
+        var last_x = Math.round(w/cs);
+        var last_y = Math.round(h/cs);
+
+        iff(this.cells[0].y=0 || this.cells[0].x < 0 || this.cells[0] > last_x || this.cells[0].y > last_y)
+        game_over = true;
+    }
   
     };
        
@@ -74,13 +97,35 @@ function draw(){
     //erase the old frame
    pen.clearRect(0,0,w,h); 
     snake.drawSnake();
+    pen.fillStyle = "yellow"
+    pen.drawImage(food_img,food.x*cs,food.y*cs,cs,cs);
+
+    pen.drawImage(trophy,18,20,cs,cs);
+    pen.fillStyle = "blue";
+    pen.font = "20px Roboto";
+    pen.fillText(score,50,50);
 }
 function update(){
 
     snake.updateSnake();
     
 }
+function getRandomFood(){
+       var foodX = Math.round(Math.random()*(w-cs)/cs);
+       var foodY = Math.round(Math.random()*(h-cs)/cs);
+       var food = {
+           x:foodX,
+           y:foodY,
+           color:"red",
+       }
+       return food;
+}
 function gameloop(){
+    if(game_over == true){
+        clearInterval(f);
+        alert("Game over");
+        return;
+    }
     draw();
     update();
 }
